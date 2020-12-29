072ed2087853e0408f80915fca5b2a3acf3ce0fe
MeeraMeera1 <gatesameera98@gmail.com>
2020-12-28 19:55:50 -0500

    test api route set up

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api',apiRouter);
 
 //create a test route 
 router.get('/hello/world', function(req,res) {
